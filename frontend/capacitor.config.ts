678b87fd85b077fcbe57aa4bbc70594f324c1ef8
Innocent Matur <maturinnocent@gmail.com>
2026-09-14 04:28:27 +0100

    feat: Add initial frontend setup with configuration files and HTML structure

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.cenexa',
+  appName: 'Cenexa Exam App',
+  webDir: 'dist'
+};
+
+export default config;
